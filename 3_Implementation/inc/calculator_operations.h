af1195ac4937905f02f24c07c05d1d648e2a9872
anandkumar1998 <31066215+anandkumar1998@users.noreply.github.com>
2021-04-16 15:32:10 +0530

    Update calculator_operations.h

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -1,47 +1,106 @@
 /** 
-* @file calculator_operations.h
+* @file studentrecord.h
 * Calculator application with 4 mathematical operations
 *
 */
-#ifndef __CALCULATOR_OPERATIONS_H__
-#define __CALCULATOR_OPERATIONS_H__
+#ifndef __STUDENTRECORD_H__
+#define __STUDENTRECORD_H__
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio_ext.h>
+//#include <stdio_ext.h>
 
 /**
 *  adds the operand1 and operand2 and returns the result
 * @param[in] operand1 
-* @param[in] operand2 
 * @return Result of operand1 + operand2
 */
-int add(int operand1, int operand2);
+int is_Exists(const char *operand1);
 
 /**
 *  subtracts the operand1, operand2 and returns the result
 * @param[in] operand1 
-* @param[in] operand2 
 * @return Result of operand1 - operand2
 */
-int subtract(int operand1, int operand2);
+void printMessageCenter(const char* operand1);
 
 /**
 *  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
+ @param[in] operand1 
 * @return Result of operand1 * operand2
 */
-int multiply(int operand1, int operand2);
+void headMessage(const char *operand1);
 
 
 /**
 * divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
+* 
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
 */
-int divide(int operand1, int operand2);
+void welmes();/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1  
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int isnameValid(const char *operand1);/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int IsLeapYear(int operand1);/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1  
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int isValidDate(Date *operand1);/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void add_student();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void initially();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void find_student();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void view();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void delete();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void cred_upd(void);/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void menu();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+void log_in();/**
+* divides the operand1 by operand2 and returns the result 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int main();
 
-#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ *//**
